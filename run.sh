efe68edf86b821f6e8e1065fc0104de5b6e2e783
Mick Sayson <mick@sayson.com>
2024-09-16 06:40:33 +0000

    Host->Guest SSH

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-qemu-system-x86_64 -kernel ./build/linux/arch/x86/boot/bzImage -append "root=/dev/vda" --enable-kvm -drive format=raw,file=build/buildroot/images/rootfs.ext2,if=virtio -vga qxl -m 1024M
+qemu-system-x86_64 -kernel ./build/linux/arch/x86/boot/bzImage -append "root=/dev/vda" --enable-kvm -drive format=raw,file=build/buildroot/images/rootfs.ext2,if=virtio -vga qxl -m 1024M -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22
+
