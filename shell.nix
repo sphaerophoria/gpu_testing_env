387a5148c2d9942e90dbc638e02ccc4af2ca05d8
Mick Sayson <mick@sayson.com>
2024-09-16 03:47:01 +0000

    Nix shell

---
+++ shell.nix
@@ -0,0 +1,20 @@
+let
+  pkgs = import <nixpkgs> { };
+  unstable = import
+    (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/8001cc402f61b8fd6516913a57ec94382455f5e5.tar.gz")
+    # reuse the current configuration
+    { config = pkgs.config; };
+in
+(pkgs.buildFHSUserEnv {
+  name = "kernel-build-env";
+  targetPkgs = pkgs: (with pkgs;
+    [
+      pkg-config
+      ncurses.dev
+      qemu
+      file
+      wget
+      libxcrypt
+    ]
+    ++ pkgs.linux.nativeBuildInputs);
+}).env
