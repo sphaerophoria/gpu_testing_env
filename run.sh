239a36d730d15ba50a69bbfdd831ee89b0210e2d
Mick Sayson <mick@sayson.com>
2024-09-17 21:25:37 +0000

    Enable kvm + host cpu settings

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-qemu-system-x86_64 -kernel ./build/linux/arch/x86/boot/bzImage -append "root=/dev/vda" --enable-kvm -drive format=raw,file=build/buildroot/images/rootfs.ext2,if=virtio -vga qxl -m 1024M -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22
+qemu-system-x86_64 -enable-kvm -cpu host -kernel ./build/linux/arch/x86/boot/bzImage -append "root=/dev/vda" --enable-kvm -drive format=raw,file=build/buildroot/images/rootfs.ext2,if=virtio -vga qxl -m 1024M -device e1000,netdev=net0 -netdev user,id=net0,hostfwd=tcp::5555-:22
 
